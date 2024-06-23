f127638da18563db8e42a9241f067b2da26a77d2
jbaldwindev <jbaldwin215@gmail.com>
2024-06-23 01:58:00 -0400

    added security config

--- demo/src/main/java/com/MovieMonster/demo/DemoApplication.java
+++ demo/src/main/java/com/MovieMonster/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.MovieMonster.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//TODO remember to remove data source exclusion
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DemoApplication {
 
 	public static void main(String[] args) {
