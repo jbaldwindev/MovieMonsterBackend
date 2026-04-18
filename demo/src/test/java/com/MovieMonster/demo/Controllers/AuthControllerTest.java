59918a6ec6ecee40f00cf390de39a0f876f2ef23
Justin <jbaldwin215@gmail.com>
2026-04-18 11:38:51 -0400

    test suite fix

--- demo/src/test/java/com/MovieMonster/demo/Controllers/AuthControllerTest.java
+++ demo/src/test/java/com/MovieMonster/demo/Controllers/AuthControllerTest.java
@@ -4,56 +4,60 @@ import com.MovieMonster.demo.Repositories.RoleRepository;
 import com.MovieMonster.demo.Repositories.UserRepository;
 import com.MovieMonster.demo.Security.JWTGenerator;
 import com.MovieMonster.demo.Services.MovieService;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(
-        controllers = AuthController.class,
-        excludeAutoConfiguration = {
-                SecurityAutoConfiguration.class,
-                SecurityFilterAutoConfiguration.class,
-                UserDetailsServiceAutoConfiguration.class
-        }
-)
-@AutoConfigureMockMvc(addFilters = false)
-@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 class AuthControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
     private UserRepository userRepository;
 
-    @MockBean
-    private RoleRepository roleRepository;
-
-    @MockBean
-    private MovieService movieService;
-
-    @MockBean
+    @Mock
     private AuthenticationManager authenticationManager;
 
-    @MockBean
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Mock
     private PasswordEncoder passwordEncoder;
 
-    @MockBean
+    @Mock
     private JWTGenerator jwtGenerator;
+
+    @Mock
+    private MovieService movieService;
+
+    @InjectMocks
+    private AuthController authController;
+
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(authController, "cookieSecure", false);
+        ReflectionTestUtils.setField(authController, "cookieSameSite", "Lax");
+        ReflectionTestUtils.setField(authController, "cookieDomain", "");
+        ReflectionTestUtils.setField(authController, "movieService", movieService);
+        mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
+    }
 
     @Test
     void meReturnsUnauthorizedWhenAuthenticationIsMissing() throws Exception {
@@ -73,5 +77,17 @@ class AuthControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(cookie().maxAge("accessToken", 0))
                 .andExpect(cookie().maxAge("refreshToken", 0));
+    }
+
+    @Test
+    void refreshReturnsOkWhenRefreshTokenIsValid() throws Exception {
+        when(jwtGenerator.validateToken("refresh-token")).thenReturn(true);
+        when(jwtGenerator.getUsernameFromJWT("refresh-token")).thenReturn("monster");
+        when(jwtGenerator.generateTokenFromUsername("monster", 900000)).thenReturn("access-token");
+
+        mockMvc.perform(post("/api/auth/refresh").cookie(new Cookie("refreshToken", "refresh-token")))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""))
+                .andExpect(cookie().exists("accessToken"));
     }
 }
--- demo/src/test/java/com/MovieMonster/demo/Controllers/UserControllerTest.java
+++ demo/src/test/java/com/MovieMonster/demo/Controllers/UserControllerTest.java
@@ -1,43 +1,39 @@
 package com.MovieMonster.demo.Controllers;
 
 import com.MovieMonster.demo.Services.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(
-        controllers = UserController.class,
-        excludeAutoConfiguration = {
-                SecurityAutoConfiguration.class,
-                SecurityFilterAutoConfiguration.class,
-                UserDetailsServiceAutoConfiguration.class
-        }
-)
-@AutoConfigureMockMvc(addFilters = false)
-@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 class UserControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
     private UserService userService;
 
+    @InjectMocks
+    private UserController userController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
+
     @Test
-    void usernameExistsEndpointReturnsServiceValueForAuthenticatedUser() throws Exception {
+    void usernameExistsEndpointReturnsServiceValue() throws Exception {
         when(userService.isUsernameTaken("monster")).thenReturn(ResponseEntity.ok(true));
 
         mockMvc.perform(get("/api/user/auth/user-exists/monster"))
